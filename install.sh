65057c726456cc40230257a55942217505ba222f
Ben Shepheard <ben@dynamictypes.com>
2015-10-01 09:50:40 +0100

    Fix sublime preferences linking

--- install.sh
+++ install.sh
@@ -10,8 +10,8 @@ EOF
 function symlink {
   name=$1
   target=${2:-"$HOME/.$name"}
-  if [ -e $target ]; then
-    if [ ! -L $target ]; then
+  if [ -e "$target" ]; then
+    if [ ! -L "$target" ]; then
       echo "WARNING: $target exists but is not a symlink."
     fi
   else
@@ -32,4 +32,4 @@ for name in $files; do
   symlink $name
 done
 
-symlink Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text \3/Packages/User/Preferences.sublime-settings
+symlink Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
