1238310f6ebfc0f1ba16855efad8a5369591b313
Ben Shepheard <ben@dynamictypes.com>
2017-01-19 21:57:08 +0000

    Add zsh, iterm and git scripts

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ln -snf "${DOTFILES_DIR}/zsh/zfunctions.symlink" "${HOME}/.zfunctions"
+ln -snf "${DOTFILES_DIR}/zsh/zshrc.symlink" "${HOME}/.zshrc"
+
+ln -snf "${DOTFILES_DIR}/git/gitconfig.symlink" "${HOME}/.gitconfig"
+ln -snf "${DOTFILES_DIR}/git/gitignore.symlink" "${HOME}/.gitignore"
+
+cp "${DOTFILES_DIR}/iterm/com.googlecode.iterm2.plist" "${HOME}/Library/Preferences/"
