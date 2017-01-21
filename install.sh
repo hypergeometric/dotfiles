d8d00ec1f65305381246e29dc7d20ef7ab1d1581
Ben Shepheard <ben@dynamictypes.com>
2017-01-21 22:33:51 +0000

    Change iterm config

--- install.sh
+++ install.sh
@@ -8,6 +8,4 @@ ln -snf "${DIRNAME}/zsh/zshrc.symlink" "${HOME}/.zshrc"
 ln -snf "${DIRNAME}/git/gitconfig.symlink" "${HOME}/.gitconfig"
 ln -snf "${DIRNAME}/git/gitignore.symlink" "${HOME}/.gitignore"
 
-cp "${DIRNAME}/iterm/com.googlecode.iterm2.plist" "${HOME}/Library/Preferences/"
-
 ln -snf "${DIRNAME}/editorconfig/editorconfig.symlink" "${HOME}/.editorconfig"
---
+++ iterm/install.sh
@@ -0,0 +1,2 @@
+defaults write com.googlecode.iterm2 "PrefsCustomFolder" -string "$DOTFILES/iterm"
+defaults write com.googlecode.iterm2 "LoadPrefsFromCustomFolder" -bool true
