d28a7193410d4d53a162397fe0ef78ee081e961a
Ben Shepheard <ben@dynamictypes.com>
2017-01-21 11:36:10 +0000

    Use dirname

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIRNAME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ln -snf "${DOTFILES_DIR}/zsh/zfunctions.symlink" "${HOME}/.zfunctions"
-ln -snf "${DOTFILES_DIR}/zsh/zshrc.symlink" "${HOME}/.zshrc"
+ln -snf "${DIRNAME}/zsh/zfunctions.symlink" "${HOME}/.zfunctions"
+ln -snf "${DIRNAME}/zsh/zshrc.symlink" "${HOME}/.zshrc"
 
-ln -snf "${DOTFILES_DIR}/git/gitconfig.symlink" "${HOME}/.gitconfig"
-ln -snf "${DOTFILES_DIR}/git/gitignore.symlink" "${HOME}/.gitignore"
+ln -snf "${DIRNAME}/git/gitconfig.symlink" "${HOME}/.gitconfig"
+ln -snf "${DIRNAME}/git/gitignore.symlink" "${HOME}/.gitignore"
 
-cp "${DOTFILES_DIR}/iterm/com.googlecode.iterm2.plist" "${HOME}/Library/Preferences/"
+cp "${DIRNAME}/iterm/com.googlecode.iterm2.plist" "${HOME}/Library/Preferences/"
 
-ln -snf "${DOTFILES_DIR}/editorconfig/editorconfig.symlink" "${HOME}/.editorconfig"
+ln -snf "${DIRNAME}/editorconfig/editorconfig.symlink" "${HOME}/.editorconfig"
