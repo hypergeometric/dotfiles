1281dc5f284529470e6be052563b6a1a05668cc5
Ben Shepheard <ben@dynamictypes.com>
2017-01-30 18:57:05 +0000

    Remove .zfunctions

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 DIRNAME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ln -snf "${DIRNAME}/zsh/zfunctions.symlink" "${HOME}/.zfunctions"
 ln -snf "${DIRNAME}/zsh/zshrc.symlink" "${HOME}/.zshrc"
 
 ln -snf "${DIRNAME}/git/gitconfig.symlink" "${HOME}/.gitconfig"
