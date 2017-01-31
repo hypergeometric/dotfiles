a18878fdb0b3718873014315fadba9c3c5c8983e
Ben Shepheard <ben@dynamictypes.com>
2017-01-31 21:55:28 +0000

    Update compinit

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,10 +1,20 @@
+# https://gist.github.com/ctechols/ca1035271ad134841284
+#
+# On slow systems, checking the cached .zcompdump file to see if it must be
+# regenerated adds a noticable delay to zsh startup.  This little hack restricts
+# it to once a day.  It should be pasted into your own completion file.
+#
+# The globbing is a little complicated here:
+# - '#q' is an explicit glob qualifier that makes globbing work within zsh's [[ ]] construct.
+# - 'N' makes the glob pattern evaluate to nothing when it doesn't match (rather than throw a globbing error)
+# - '.' matches "regular files"
+# - 'mh+24' matches files (or directories or whatever) that are older than 24 hours.
 autoload -Uz compinit
-typeset -i updated_at=$(date +'%j' -r ~/.zcompdump 2>/dev/null || stat -f '%Sm' -t '%j' ~/.zcompdump 2>/dev/null)
-if [ $(date +'%j') != $updated_at ]; then
-  compinit
+if [[ -n ${ZDOTDIR}/.zcompdump(#qN.mh+24) ]]; then
+  compinit;
 else
-  compinit -C
-fi
+  compinit -C;
+fi;
 
 # Force rehash when command not found
 _force_rehash() {
